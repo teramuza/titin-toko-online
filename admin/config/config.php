93210e44954f727aed0838d43149edf3b3f83080
Teuku Raja <teramuza@gmail.com>
2022-07-05 17:03:01 +0700

    move admin to admin folder

---
+++ admin/config/config.php
@@ -0,0 +1,18 @@
+<?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/***
+ * DATABASE CONFIGURATION
+ * CALL USING $DB_CONFIG
+ */
+$DB_CONFIG['HOST'] = 'localhost';
+$DB_CONFIG['USERNAME'] = 'develop';
+$DB_CONFIG['PASSWORD'] = 'min1011';
+$DB_CONFIG['NAME'] = 'db_public_toko_online';
+
+/***
+ * SERVER CONFIGURATION
+ * CALL USING $SERVER_CONFIG
+ */
+$SERVER_CONFIG['USING_SSL'] = false;
+$SERVER_CONFIG['DEV_MODE'] = true;
